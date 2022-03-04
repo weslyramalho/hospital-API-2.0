a10627736a4a4cac0b7325d29f34b1e44c86f913
Wesly <weslyramalho@yahoo.com.br>
2022-03-04 19:20:53 -0300

    pacienteService

---
+++ src/main/java/com/teste/teste/services/PacienteService.java
@@ -0,0 +1,27 @@
+package com.teste.teste.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teste.teste.entities.Paciente;
+import com.teste.teste.repositories.PacienteRepository;
+
+@Service
+public class PacienteService {
+	
+	@Autowired
+	private PacienteRepository repository;
+	
+	public List<Paciente> findAll(){
+		return repository.findAll();
+	}
+	
+	public Paciente findById(Long id) {
+		Optional<Paciente> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
