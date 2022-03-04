426056404df8e7400f42590996c912fc6377cb25
Wesly <weslyramalho@yahoo.com.br>
2022-03-04 18:40:59 -0300

    historicoResource

---
+++ src/main/java/com/teste/teste/resources/HistoricoResource.java
@@ -0,0 +1,23 @@
+package com.teste.teste.resources;
+
+import java.time.Instant;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.teste.entities.Funcionario;
+import com.teste.teste.entities.Historico;
+
+@RestController
+@RequestMapping(value = "/historico")
+public class HistoricoResource {
+	
+	@GetMapping
+	public ResponseEntity<Historico> findAll(){
+		Historico h1 = new Historico(null, Instant.parse("2019-06-20T19:53:07Z"), Instant.parse("2019-07-20T19:53:07Z"), "infecção instetinal", 1L, 1L ); 
+		return ResponseEntity.ok().body(h1);
+	}
+
+}
