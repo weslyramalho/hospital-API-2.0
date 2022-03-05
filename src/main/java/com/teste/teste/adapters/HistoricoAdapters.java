85ff68be514d3d94d0ff93547bee7ace609c256e
Wesly <weslyramalho@yahoo.com.br>
2022-03-05 17:56:04 -0300

    hospitalização por pacientes

--- src/main/java/com/teste/teste/adapters/HistoricoAdapters.java
+++ src/main/java/com/teste/teste/adapters/HistoricoAdapters.java
@@ -7,12 +7,14 @@ import org.aspectj.weaver.NewConstructorTypeMunger;
 
 import com.teste.teste.entities.Funcionario;
 import com.teste.teste.entities.Historico;
+import com.teste.teste.entities.HospitalizacaoPorPaciente;
 import com.teste.teste.entities.MedicosPorDepartamento;
+import com.teste.teste.entities.Paciente;
 
 public class HistoricoAdapters {
 	
 	
-	public List<MedicosPorDepartamento> medicosPorDepartamento(List<Historico> historicos, List<Funcionario> funcionarios){
+	public List<MedicosPorDepartamento> adMedicosPorDepartamento(List<Historico> historicos, List<Funcionario> funcionarios){
 		List<MedicosPorDepartamento> medicosPorDepartamentos =funcionarios.stream().map(f -> {
 			List<Historico> historicoList = historicos.stream().filter(h -> h.getPaciente().getId().equals(f.getId()))
 					.collect(Collectors.toList());
@@ -21,5 +23,17 @@ public class HistoricoAdapters {
 		}).collect(Collectors.toList());
 		return medicosPorDepartamentos;
 	}
+	
+	public List<HospitalizacaoPorPaciente> adHospitalizacaoPorPaciente(List<Historico> historicos, List<Paciente> pacientes){
+		List<HospitalizacaoPorPaciente> hospitalizacaoPorPacientes =pacientes.stream().map(p -> {
+			List<Historico> historicoList = historicos.stream().filter(h -> h.getPaciente().getId().equals(p.getId()))
+					.collect(Collectors.toList());
+			
+			return new HospitalizacaoPorPaciente(p, historicoList);
+		}).collect(Collectors.toList());
+		return hospitalizacaoPorPacientes;
+	}
+	
+	
 
 }
