2be4bc581cdd2d9e988e25be1094828ff7d4e11f
Wesly <weslyramalho@yahoo.com.br>
2022-03-07 18:45:27 -0300

    ajustes

--- src/main/java/com/teste/teste/resources/PacienteResource.java
+++ src/main/java/com/teste/teste/resources/PacienteResource.java
@@ -1,22 +1,65 @@
 package com.teste.teste.resources;
 
+import java.net.URI;
 import java.time.Instant;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.teste.teste.entities.Funcionario;
+import com.teste.teste.entities.Paciente;
+import com.teste.teste.services.FuncionarioService;
+import com.teste.teste.services.PacienteService;
 import com.teste.teste.entities.Paciente;
 
 @RestController
 @RequestMapping(value = "/pacientes")
 public class PacienteResource {
 	
+	@Autowired
+	private PacienteService service;
+	
 	@GetMapping
-	public ResponseEntity<Paciente> findAll(){
-		Paciente u1 = new Paciente(null, "João Miguel lopes", Instant.parse("2015-05-22T15:21:22Z"), "45678925454"); 
-		return ResponseEntity.ok().body(u1);
+	public ResponseEntity<List<Paciente>> findAll(){
+		List<Paciente> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Paciente> findById(@PathVariable Long id){
+		Paciente obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Paciente> insert(@RequestBody Paciente obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Paciente> update(@PathVariable Long id, @RequestBody Paciente obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+				
+	}
+
 
 }
--- src/main/java/com/teste/teste/services/PacienteService.java
+++ src/main/java/com/teste/teste/services/PacienteService.java
@@ -7,8 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.teste.teste.entities.Paciente;
-import com.teste.teste.entities.Paciente;
-import com.teste.teste.entities.Paciente;
+
 import com.teste.teste.repositories.PacienteRepository;
 
 @Service
