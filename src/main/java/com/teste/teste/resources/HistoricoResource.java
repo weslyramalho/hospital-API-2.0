67b15e2f94cd590d5d804bc1403f7aeda65248ba
Wesly <weslyramalho@yahoo.com.br>
2022-03-07 18:33:47 -0300

    implementado metodos de update, delete e insert

--- src/main/java/com/teste/teste/resources/HistoricoResource.java
+++ src/main/java/com/teste/teste/resources/HistoricoResource.java
@@ -1,15 +1,22 @@
 package com.teste.teste.resources;
 
+import java.net.URI;
 import java.time.Instant;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.teste.teste.entities.Funcionario;
+import com.teste.teste.entities.Historico;
 import com.teste.teste.entities.Historico;
 import com.teste.teste.entities.Paciente;
 import com.teste.teste.services.HistoricoService;
@@ -26,5 +33,33 @@ public class HistoricoResource {
 		List<Historico> list= service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Historico> findById(@PathVariable Long id){
+		Historico obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Historico> insert(@RequestBody Historico obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Historico> update(@PathVariable Long id, @RequestBody Historico obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+				
+	}
+
 
 }
