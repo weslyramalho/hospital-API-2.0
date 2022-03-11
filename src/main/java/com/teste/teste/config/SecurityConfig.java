ec01360852ff0c0db4ed9e0cb80ae091d3585fc7
Wesly <weslyramalho@yahoo.com.br>
2022-03-11 19:19:35 -0300

    jwt

---
+++ src/main/java/com/teste/teste/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.teste.teste.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Env;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+			
+	};
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/funcionarios/**",
+			"/pacientes/**"
+	};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+	
+		
+		http.cors();
+		http.authorizeRequests()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
+		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return (CorsConfigurationSource) source;
+	}
+	
+	
+
+}
